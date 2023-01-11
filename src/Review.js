67ff5ba98c3be0a65e8b97252181612d5bdc163f
white3d <odhiambo.edward111@gmail.com>
2023-01-11 11:41:35 +0300

    Add React Icons

---
+++ src/Review.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import people from './data';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+
+const Review = () => {
+  const [index, setIndex] = useState(0)
+  const {name, image, job, text} = people[index]
+  return (
+    <article className="review">
+      <div className="img-container">
+        <img src={image} alt={name} className="person-img" />
+        <span className='quote-icon'>
+          <FaQuoteRight />
+        </span>
+      </div>
+    </article>
+  );
+};
+
+export default Review;
